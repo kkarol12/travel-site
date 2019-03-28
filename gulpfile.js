692c1ccf04f61d7a21de52426646a20beab37bce
Karol <kkarol2@wp.pl>
2019-03-28 21:52:47 +0100

    Starting divide css to smaler files

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function(){
   console.log("Hooray - you created a gulp task");
@@ -15,15 +16,15 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([nested, cssvars, autoprefixer]))
+    .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
 
-  watch('./app/index.html', function(){
-    gulp.start('html');
-  });
+  // watch('./app/index.html', function(){
+  //   gulp.start('html');
+  // });
 
   watch('./app/assets/styles/**/*.css', function(){
     gulp.start('styles');
