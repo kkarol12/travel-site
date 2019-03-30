103e40df24cb0181c1ab29813fc9f910197466d3
Karol <kkarol2@wp.pl>
2019-03-30 18:37:50 +0100

    Files after section 5 and 6

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, nested, cssvars, autoprefixer])) //przetwarza w tym miejcu przez poszczegolne moduły
+    .on('error', function(errorInfo){
+      console.log(errorInfo.toString());
+      this.emit('end'); //w tym przypadku gulp bedzie myslal ze tak ma sie zkonczyczyc funkcja w przypadku błedu i nie zakonczy watcha w konsoli
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
