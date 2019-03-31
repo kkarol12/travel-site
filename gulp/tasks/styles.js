6f196f970d81ae2217915bf3866eedde7a1eb86b
Karol <kkarol2@wp.pl>
2019-03-31 17:46:52 +0200

    Files after section 8

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, nested, cssvars, autoprefixer])) //przetwarza w tym miejcu przez poszczegolne moduły
+    .pipe(postcss([cssImport, mixins, nested, cssvars, autoprefixer])) //przetwarza w tym miejcu przez poszczegolne moduły
     .on('error', function(errorInfo){
       console.log(errorInfo.toString());
       this.emit('end'); //w tym przypadku gulp bedzie myslal ze tak ma sie zkonczyczyc funkcja w przypadku błedu i nie zakonczy watcha w konsoli
